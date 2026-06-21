cad8700a647d245604216ed59f3d605600863478
hentostc06 <hentostc06@gmail.com>
2026-06-22 00:00:23 +0700

    fix fix server

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Carbon\CarbonImmutable;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
@@ -24,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         FileUpload::configureUsing(function (FileUpload $component): void {
             $component
                 ->deletable(true)
